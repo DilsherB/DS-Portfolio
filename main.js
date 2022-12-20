d46d9432f75039b3faadfce483d5b6f5398b47e8
Dilsher Balouch <dilsherb@gmail.com>
2022-12-20 15:46:22 +0300

    linter

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const navLinks = document.querySelectorAll('.nav-item');
 const menuToggle = document.getElementById('collapsibleNavbar');
 
-// eslint-disable-next-line no-no-undef
+// eslint-disable-next-line no-undef
 const bsCollapse = new bootstrap.Collapse(menuToggle);
 navLinks.forEach((n) => {
   n.addEventListener('click', () => { bsCollapse.toggle(); });
