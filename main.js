56a04b762898493ac241f733a818aff1e239e2ba
Dilsher Balouch <dilsherb@gmail.com>
2022-12-20 14:38:06 +0300

    linter issues resolved.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('collapsibleNavbar')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('collapsibleNavbar');
+const bsCollapse = new bootstrap.Collapse(menuToggle);
 navLinks.forEach((n) => {
-    n.addEventListener('click', () => { bsCollapse.toggle() })
-})
+  n.addEventListener('click', () => { bsCollapse.toggle(); });
+});
