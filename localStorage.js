2e1416f36343dd9cde983eec0c55f27c84f3ca32
Dilsher Balouch <41000844+DilsherB@users.noreply.github.com>
2023-01-02 15:01:59 +0300

    update local storage
    populating even w/o submitting

--- localStorage.js
+++ localStorage.js
@@ -1,19 +1,23 @@
-const fields = document.querySelectorAll('.formData');
+<<<<<<< HEAD
+const fields = document.querySelectorAll(".formData");
 const localData = {
-  fName: '',
-  lName: '',
-  email: '',
-  msg: '',
+  fName: "",
+  lName: "",
+  email: "",
+  msg: "",
 };
 fields.forEach((field) => {
-  field.addEventListener('input', () => {
+  field.addEventListener("input", () => {
     localData[field.name] = field.value;
-    localStorage.setItem('contactFormData', JSON.stringify(localData));
+    localStorage.setItem("contactFormData", JSON.stringify(localData));
   });
 });
-const retrievedData = JSON.parse(localStorage.getItem('contactFormData'));
-window.addEventListener('load', () => {
-  fields.forEach((field) => {
-    field.value = retrievedData[field.name];
-  });
-});
+window.addEventListener("load", () => {
+  let retrievedData = {};
+  if (localStorage.getItem("contactFormData")) {
+    retrievedData = JSON.parse(localStorage.getItem("contactFormData"));
+    document.getElementById("fName").value = retrievedData.fName;
+    document.getElementById("lName").value = retrievedData.lName;
+    document.getElementById("email").value = retrievedData.email;
+    document.getElementById("msg").value = retrievedData.msg;
+  }
