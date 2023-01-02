847c18c2136fb0c689f1387bad856c2de59d0950
Dilsher Balouch <41000844+DilsherB@users.noreply.github.com>
2023-01-02 15:15:29 +0300

    linters

--- localStorage.js
+++ localStorage.js
@@ -1,23 +1,23 @@
-const fields = document.querySelectorAll(".formData");
+const fields = document.querySelectorAll('.formData');
 const localData = {
-  fName: "",
-  lName: "",
-  email: "",
-  msg: "",
+  fName: '',
+  lName: '',
+  email: '',
+  msg: '',
 };
 fields.forEach((field) => {
-  field.addEventListener("input", () => {
+  field.addEventListener('input', () => {
     localData[field.name] = field.value;
-    localStorage.setItem("contactFormData", JSON.stringify(localData));
+    localStorage.setItem('contactFormData', JSON.stringify(localData));
   });
 });
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   let retrievedData = {};
-  if (localStorage.getItem("contactFormData")) {
-    retrievedData = JSON.parse(localStorage.getItem("contactFormData"));
-    document.getElementById("fName").value = retrievedData.fName;
-    document.getElementById("lName").value = retrievedData.lName;
-    document.getElementById("email").value = retrievedData.email;
-    document.getElementById("msg").value = retrievedData.msg;
-  };
-})
+  if (localStorage.getItem('contactFormData')) {
+    retrievedData = JSON.parse(localStorage.getItem('contactFormData'));
+    document.getElementById('fName').value = retrievedData.fName;
+    document.getElementById('lName').value = retrievedData.lName;
+    document.getElementById('email').value = retrievedData.email;
+    document.getElementById('msg').value = retrievedData.msg;
+  }
+});
